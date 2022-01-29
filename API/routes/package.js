3eefe6a28ba32d32e799b87df087725bd2789d45
Karolina K <kaczmarska.kar@gmail.com>
2022-01-29 20:15:04 +0000

    clean up

--- API/routes/package.js
+++
@@ -1,2 +0,0 @@
-//i need a route to create a parcel
-//schema for package table: id, account id, truck_id , weight , last_truck_id??
--- API/routes/truck.js
+++
@@ -1,8 +0,0 @@
-// i need a route for creating new truck - post
-// i need a route to load the truck with parcels - post./truck/id/load
-//i need a route to unload the truck - post ./trucks/id/unload ??
-// i need a route to get the number of parcels in each truck get ./trucks/id/parcels- truck.parcels.length
-// i need a route to get the weight of the truck based on how many parcels are loaded get ./trucks/id/weight
-// and the weight will be: parcels.map
-
-//schema for trucks table: id,account_id, weight,parcels- an array of parcel ids
